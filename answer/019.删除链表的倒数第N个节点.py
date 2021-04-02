9e46926495b3a9a81df28557b52b83dcd7527659
mecthew <905843145@qq.com>
2021-04-02 19:50:44 +0800

    finish 019

--- answer/019.删除链表的倒数第N个节点.py
+++ answer/019.删除链表的倒数第N个节点.py
@@ -1,1 +1,30 @@
 from typing import List
+# Medium
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy_node = ListNode(next=head)
+        prev = dummy_node
+        cur = dummy_node
+
+        while n and prev.next:
+            prev = prev.next
+            n -= 1
+
+        while prev.next:
+            cur = cur.next
+            prev = prev.next
+
+        if cur:
+            tmp = cur.next
+            cur.next = cur.next.next
+            del tmp
+        return dummy_node.next
+
+
