fa70152d5ba5ddb46c07c1f62e69e5b8f98a892e
mecthew <905843145@qq.com>
2021-04-02 19:50:56 +0800

    finish 020

--- answer/020.有效的括号.py
+++ answer/020.有效的括号.py
@@ -1,1 +1,22 @@
 from typing import List
+
+
+# Easy
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        parentheses_dict = {')': '(', ']': '[', '}': '{'}
+        for ch in s:
+            if ch in parentheses_dict.keys():
+                if len(stack) < 1 or stack[-1] != parentheses_dict[ch]:
+                    return False
+                else:
+                    stack.pop(-1)
+            else:
+                stack.append(ch)
+        return not stack
+
+
+sol = Solution()
+print(sol.isValid('[)'))
