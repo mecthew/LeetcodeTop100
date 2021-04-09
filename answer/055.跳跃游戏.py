084a1c751f25fb0b8a4dacadac9c0cc775fb0afc
mecthew <905843145@qq.com>
2021-04-09 18:53:41 +0800

    finish 055-056

--- answer/055.跳跃游戏.py
+++ answer/055.跳跃游戏.py
@@ -1,1 +1,17 @@
 from typing import List
+# Medium
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        N = len(nums)
+        furthest_idx = 0
+
+        for ith, num in enumerate(nums):
+            if furthest_idx < ith:
+                return False
+
+            furthest_idx = max(furthest_idx, ith+num)
+            if furthest_idx >= N-1:
+                return True
+
+        return furthest_idx >= N-1
--- answer/056.合并区间.py
+++ answer/056.合并区间.py
@@ -1,1 +1,20 @@
 from typing import List
+# Medium
+
+"""
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+"""
+
+# 排序 + 看前一位
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        merge = []
+        for interval in intervals:
+            if not merge or merge[-1][1] < interval[0]:
+                merge.append(interval)
+            elif interval[1] > merge[-1][1]:
+                merge[-1][1] = interval[1]
+        return merge
