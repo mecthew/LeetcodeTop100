0ebc397291c479ea49bc9837ab6ddeac82e0bf00
mecthew <905843145@qq.com>
2021-04-09 19:12:25 +0800

    finish 062-064

--- answer/062.不同路径.py
+++ answer/062.不同路径.py
@@ -1,1 +1,11 @@
 from typing import List
+# Medium，从矩阵左上角到右下角的路径数
+class Solution:
+    # 空间可优化
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+
+        for i in range(m-2, -1, -1):
+            for j in range(n-1, -1, -1):
+                dp[j] = dp[j] if j == n-1 else dp[j] + dp[j+1]
+        return dp[0]
--- answer/064.最小路径和.py
+++ answer/064.最小路径和.py
@@ -1,1 +1,14 @@
 from typing import List
+# Medium,与62相似
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [0] * n
+        for j in range(n-1, -1, -1):
+            dp[j] = grid[-1][j] if j == n-1 else dp[j+1] + grid[-1][j]
+
+        for i in range(m-2, -1, -1):
+            for j in range(n-1, -1, -1):
+                dp[j] = grid[i][j] + (dp[j] if j == n-1 else min(dp[j], dp[j+1]))
+        return dp[0]
