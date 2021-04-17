a52d01478dfa8b73b6370126445d313101fc66f2
mecthew <905843145@qq.com>
2021-04-17 10:18:33 +0800

    finish 094-096

--- answer/094.二叉树的中序遍历.py
+++ answer/094.二叉树的中序遍历.py
@@ -1,1 +1,23 @@
 from typing import List
+# Medium
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ret = []
+
+        def inorder_visit(root_node):
+            if root_node is None:
+                return
+            else:
+                inorder_visit(root_node.left)
+                ret.append(root_node.val)
+                inorder_visit(root_node.right)
+
+        inorder_visit(root)
+        return ret
--- answer/096.不同的二叉搜索树.py
+++ answer/096.不同的二叉搜索树.py
@@ -1,1 +1,16 @@
 from typing import List
+# Medium
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [1] * (n+1)
+        for i in range(2, n+1):
+            num = 0
+            for j in range(1, i+1):
+                num += dp[j-1] * dp[i-j]
+            dp[i] = num
+        return dp[n]
+
+
+sol = Solution()
+print(sol.numTrees(1))
