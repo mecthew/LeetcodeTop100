dde3fa7a847eb891d767abeebee8f7e01b7179da
mecthew <905843145@qq.com>
2021-04-18 10:42:51 +0800

    finish 098-101

--- answer/098.验证二叉搜索树.py
+++ answer/098.验证二叉搜索树.py
@@ -1,1 +1,35 @@
 from typing import List
+import sys
+# Medium
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    prev_val = -sys.maxsize
+
+    def isValidBST(self, root: TreeNode) -> bool:
+
+        def dfs_search(root_node):
+            if root_node.left is None and root_node.right is None:
+                flag = self.prev_val < root_node.val
+                self.prev_val = root_node.val
+                return flag
+            else:
+                if root_node.left:
+                    flag = dfs_search(root_node.left)
+                    if not flag:
+                        return False
+
+                if self.prev_val >= root_node.val:
+                    return False
+                self.prev_val = root_node.val
+                if root_node.right:
+                    flag = dfs_search(root_node.right)
+                    if not flag:
+                        return False
+                return True
+        return dfs_search(root)
--- answer/101.对称二叉树.py
+++ answer/101.对称二叉树.py
@@ -1,1 +1,25 @@
 from typing import List
+# Easy
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def checkSymmetric(node1, node2):
+            if node1 is None and node2 is None:
+                return True
+            elif node1 is None or node2 is None:
+                return False
+            else:
+                if not checkSymmetric(node1.left, node2.right):
+                    return False
+
+                if node1.val != node2.val:
+                    return False
+
+                return checkSymmetric(node1.right, node2.left)
+
+        return checkSymmetric(root.left, root.right)
