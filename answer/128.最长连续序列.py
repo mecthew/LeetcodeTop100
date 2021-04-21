8562d5030128720a460b7c617a8ba3e1c89b326d
mecthew <905843145@qq.com>
2021-04-21 11:12:13 +0800

    finish 128-136

--- answer/128.最长连续序列.py
+++ answer/128.最长连续序列.py
@@ -1,1 +1,18 @@
 from typing import List
+
+# Hard
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        len_dict = {}
+        max_len = 0
+        for num in nums:
+            if num not in len_dict:
+                left_len = len_dict.get(num-1, 0)
+                right_len = len_dict.get(num+1, 0)
+                cur_len = left_len + 1 + right_len
+                max_len = max(max_len, cur_len)
+                len_dict[num] = cur_len
+                len_dict[num-left_len] = cur_len
+                len_dict[num+right_len] = cur_len
+
+        return max_len
--- answer/136.只出现一次的数字.py
+++ answer/136.只出现一次的数字.py
@@ -1,1 +1,9 @@
 from typing import List
+# Easy
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor_sum = 0
+        for num in nums:
+            xor_sum ^= num
+        return xor_sum
