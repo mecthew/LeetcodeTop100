270a3b7ede0f95a17be31ebccd22114e316f7896
mecthew <905843145@qq.com>
2021-04-22 15:52:18 +0800

    finish 139-141

--- answer/139.单词拆分.py
+++ answer/139.单词拆分.py
@@ -1,1 +1,17 @@
 from typing import List
+# Medium
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        n = len(s)
+
+        dp = [False] * n
+        dp.append(True)
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n+1):
+                if dp[j] and s[i: j] in word_set:
+                    dp[i] = True
+                    break
+        return dp[0]
+
+
--- answer/141.环形链表.py
+++ answer/141.环形链表.py
@@ -1,1 +1,18 @@
 from typing import List
+# Easy
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return False
+        fast_p, low_p = head, head
+        while fast_p and fast_p.next:
+            low_p = low_p.next
+            fast_p = fast_p.next.next
+            if low_p == fast_p:
+                return True
+        return False
