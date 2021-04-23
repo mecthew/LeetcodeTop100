e6793be99942a981727a6355a493812d17f048c4
mecthew <905843145@qq.com>
2021-04-23 16:18:54 +0800

    finish 142-146

--- answer/142.环形链表Ⅱ.py
+++ answer/142.环形链表Ⅱ.py
@@ -1,1 +1,26 @@
 from typing import List
+# Medium
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow, fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+
+        if fast is None or fast.next is None:
+            return None
+        else:
+            slow2 = head
+            while slow != slow2:
+                slow = slow.next
+                slow2 = slow2.next
+
+            return slow
--- answer/146.LRU缓存机制.py
+++ answer/146.LRU缓存机制.py
@@ -1,1 +1,20 @@
 from typing import List
+from collections import OrderedDict
+# Medium
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dic = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.dic:
+            self.dic.move_to_end(key)
+        return self.dic.get(key, -1)
+
+    def put(self, key: int, value: int) -> None:
+        self.dic[key] = value
+        self.dic.move_to_end(key)
+        if len(self.dic) > self.capacity:
+            self.dic.popitem(last=False)
