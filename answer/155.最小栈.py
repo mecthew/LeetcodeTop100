c215e663285bb7608f3efc496ccea29135d11f80
mecthew <905843145@qq.com>
2021-04-26 19:14:18 +0800

    finish 155-160

--- answer/155.最小栈.py
+++ answer/155.最小栈.py
@@ -1,1 +1,24 @@
 from typing import List
+# Easy
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(self.stack[-1][1], val)))
+
+    def pop(self) -> None:
+        self.stack.pop(-1)
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
--- answer/160.相交链表.py
+++ answer/160.相交链表.py
@@ -1,1 +1,31 @@
 from typing import List
+# Easy
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# 链表常用方法：双/快慢指针，虚拟头结点
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        if not headA or not headB:
+            return None
+
+        cur1, cur2 = headA, headB
+        first_end1, first_end2 = False, False
+
+        while cur1 and cur2:
+            if cur1 == cur2:
+                return cur1
+
+            cur1 = cur1.next
+            cur2 = cur2.next
+            if cur1 is None and first_end1 is False:
+                first_end1 = True
+                cur1 = headB
+            if cur2 is None and first_end2 is False:
+                first_end2 = True
+                cur2 = headA
+
+        return None
