ba9bfd6ea71ffe094bd159a246b311028872a49f
mecthew <905843145@qq.com>
2021-04-29 11:07:23 +0800

    finish 208-215

--- answer/208.实现Trie(前缀树).py
+++ answer/208.实现Trie(前缀树).py
@@ -1,1 +1,43 @@
 from typing import List
+# Medium
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        root = self.trie
+        for ch in word:
+            if ch not in root:
+                root[ch] = {}
+            root = root[ch]
+        root['is_word'] = True
+
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        root = self.trie
+        for ch in word:
+            if ch not in root:
+                return False
+            root = root[ch]
+        return 'word' in root
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        root = self.trie
+        for ch in prefix:
+            if ch not in root:
+                return False
+            root = root[ch]
+        return True
--- answer/215.数组中的第K个最大元素.py
+++ answer/215.数组中的第K个最大元素.py
@@ -1,1 +1,18 @@
 from typing import List
+import heapq
+import sys
+# Medium
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        max_heap = [num for num in nums[:k]]
+        heapq.heapify(max_heap)
+
+        for num in nums[k:]:
+            heapq.heappushpop(max_heap, num)
+        # print(max_heap)
+        ret = heapq.heappushpop(max_heap, sys.maxsize)
+        return ret
+
+sol = Solution()
+nums = [3,2,1,5,6,4]; k = 2
+print(sol.findKthLargest(nums, k))
