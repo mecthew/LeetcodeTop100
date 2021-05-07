ba57c6f054a9ad7b39d40abd6a7c69425fc501cd
mecthew <905843145@qq.com>
2021-05-07 15:33:48 +0800

    finish 309

--- answer/309.最佳买卖股票时期含冷冻期.py
+++ answer/309.最佳买卖股票时期含冷冻期.py
@@ -1,1 +1,11 @@
 from typing import List
+# Medium
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [0] * 3
+        for ith, price in enumerate(prices):
+            if ith == 0:
+                dp[0] = -price
+            else:
+                dp[0], dp[1], dp[2] = max(dp[0], dp[1]-price), max(dp[1], dp[2]), dp[0] + price
+        return max(dp)
