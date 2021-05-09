ff49e49af3e7e013441e8684ec1b0821ffebf4c7
mecthew <905843145@qq.com>
2021-05-09 10:13:18 +0800

    finish 322-337

--- answer/322.零钱兑换.py
+++ answer/322.零钱兑换.py
@@ -1,1 +1,28 @@
 from typing import List
+# Medium
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+
+        dp = [-1] * amount
+        for a in range(amount):
+            for coin in coins:
+                if a + 1 == coin:
+                    dp[a] = 1
+                    break
+                else:
+                    if a - coin >= 0 and dp[a - coin] > 0:
+                        if dp[a] == -1:
+                            dp[a] = dp[a - coin] + 1
+                        else:
+                            dp[a] = min(dp[a], dp[a - coin] + 1)
+        # print(dp)
+        return dp[-1]
+
+
+coins = [1,2,5]
+amount = 11
+sol = Solution()
+print(sol.coinChange(coins, amount))
--- answer/337.打家劫舍Ⅲ.py
+++ answer/337.打家劫舍Ⅲ.py
@@ -1,1 +1,21 @@
 from typing import List
+# Medium
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def dfs_rob(root_node):
+            if root_node is None:
+                return 0, 0
+            else:
+                left_sum, left_child_sum = dfs_rob(root_node.left)
+                right_sum, right_child_sum = dfs_rob(root_node.right)
+
+                max_sum = max(root_node.val + left_child_sum + right_child_sum, left_sum + right_sum)
+                return max_sum, left_sum + right_sum
+        return dfs_rob(root)[0]
