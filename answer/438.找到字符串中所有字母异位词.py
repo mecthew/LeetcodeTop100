cadeca507c2d49560b25d9b0431c1aac4539a6be
mecthew <905843145@qq.com>
2021-05-15 11:35:43 +0800

    finish 438-448

--- answer/438.找到字符串中所有字母异位词.py
+++ answer/438.找到字符串中所有字母异位词.py
@@ -1,1 +1,26 @@
 from typing import List
+from collections import Counter
+
+# Medium, O(26 * n)
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        ret = []
+        counter = Counter(p)
+
+        for i in range(len(s)):
+            if i >= len(p):
+                if s[i - len(p)] in counter:
+                    counter[s[i - len(p)]] += 1
+
+            if s[i] in counter:
+                counter[s[i]] -= 1
+
+            if all(v == 0 for v in counter.values()):
+                ret.append(i - len(p) + 1)
+        return ret
+
+
+sol = Solution()
+s = "cbaebabacd"
+p = "abc"
+print(sol.findAnagrams(s, p))
--- answer/448.找到所有数组中消失的数字.py
+++ answer/448.找到所有数组中消失的数字.py
@@ -1,1 +1,14 @@
 from typing import List
+# Easy, solve it without extra space and in O(n) runtime?
+# You may assume the returned list does not count as extra space.
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for num in nums:
+            num %= len(nums) + 1
+
+            nums[num - 1] += len(nums) + 1
+
+        ret = [idx+1 for idx, num in enumerate(nums) if num < len(nums) + 1]
+        return ret
+
