a576e2ee4c82e5926d54411625642910d71b8448
mecthew <905843145@qq.com>
2021-05-20 20:07:26 +0800

    finish 581-617

--- answer/581.最短无序连续子数组.py
+++ answer/581.最短无序连续子数组.py
@@ -1,1 +1,32 @@
 from typing import List
+# Medium
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        left, right = -1, -1
+        min_val, max_val = None, None
+
+        for ith in range(len(nums)):
+            if left == -1 and ith + 1 < len(nums) and nums[ith] > nums[ith + 1]:
+                left = ith
+                right = ith + 1
+                max_val = nums[ith]
+                min_val = nums[ith + 1]
+            elif ith + 1 < len(nums) and nums[ith] > nums[ith + 1]:
+                right = ith + 1
+                max_val = max(max_val, nums[ith])
+                min_val = min(min_val, nums[ith + 1])
+                while left > 0 and min_val < nums[left - 1]:
+                    left -= 1
+            elif max_val is not None and nums[ith] < max_val:
+                right = ith
+                min_val = min(min_val, nums[ith])
+                while left > 0 and min_val < nums[left - 1]:
+                    left -= 1
+
+        return 0 if left == -1 else right - left + 1
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = [1, 3, 2, 2, 2]
+    print(sol.findUnsortedSubarray(nums))
--- answer/617.合并二叉树.py
+++ answer/617.合并二叉树.py
@@ -1,1 +1,20 @@
 from typing import List
+
+# Easy
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if root1 is None or root2 is None:
+            return root1 if root1 else root2
+        else:
+            root1.val = root1.val + root2.val
+            left = self.mergeTrees(root1.left, root2.left)
+            right = self.mergeTrees(root1.right, root2.right)
+            root1.left = left
+            root1.right = right
+            return root1
