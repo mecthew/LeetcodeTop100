2a1a346f5c4f63af9429e3d5c1cfdad2e744c540
mecthew <905843145@qq.com>
2021-05-22 16:28:00 +0800

    finish 647-771"

--- answer/647.回文子串.py
+++ answer/647.回文子串.py
@@ -1,1 +1,20 @@
 from typing import List
+
+# Medium
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        total = 0
+        dp = [[True]*n for _ in range(n)]
+
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if i == n-1:
+                    flag = True
+                elif j == 0:
+                    flag = True
+                else:
+                    flag = dp[i+1][j-1]
+                dp[i][j] = s[i] == s[j] and flag
+                total += 1 if dp[i][j] else 0
+        return total
--- answer/771.宝石与石头.py
+++ answer/771.宝石与石头.py
@@ -1,1 +1,7 @@
 from typing import List
+# Easy
+
+import operator
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        return len([1 for stone in stones if stone in set(jewels)])
